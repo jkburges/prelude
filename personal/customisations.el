b0bdc7bf5a19a3988004134e9ed38f629d2cb051
Jon Burgess <jkburges@gmail.com>
2015-09-25 12:56:53 +1000

    short cut for common comment and copy command

--- personal/customisations.el
+++ personal/customisations.el
@@ -16,5 +16,17 @@
 ;; standard width for my emacs window - 80 is not enough, what is this - 1970?
 (setq whitespace-line-column 120)
 
+;; copy and comment - http://stackoverflow.com/a/23588908/627806
+(defun copy-and-comment-region (beg end &optional arg)
+  "Duplicate the region and comment-out the copied text.
+See `comment-region' for behavior of a prefix arg."
+  (interactive "r\nP")
+  (copy-region-as-kill beg end)
+  (goto-char end)
+  (yank)
+  (comment-region beg end arg))
+
+(global-set-key (kbd "C-c c") 'copy-and-comment-region)
+
 (provide 'customisations)
 ;;; customisations.el ends here

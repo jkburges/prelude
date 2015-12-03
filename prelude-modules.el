ce767d45279204ff12be40e01fd9dd426ec52468
Jon Burgess <jkburges@gmail.com>
2015-12-04 10:56:37 +1100

    Modules in git

---
+++ prelude-modules.el
@@ -0,0 +1,15 @@
+(require 'prelude-ido) ;; Super charges Emacs completion for C-x C-f and more
+(require 'prelude-helm) ;; Interface for narrowing and search
+;; (require 'prelude-helm-everywhere) ;; Enable Helm everywhere
+(require 'prelude-company)
+(require 'prelude-c)
+(require 'prelude-coffee)
+(require 'prelude-css)
+(require 'prelude-emacs-lisp)
+(require 'prelude-js)
+(require 'prelude-ruby)
+(require 'prelude-shell)
+(require 'prelude-scss)
+(require 'prelude-web) ;; Emacs mode for web templates
+(require 'prelude-xml)
+(require 'prelude-yaml)
